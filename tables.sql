08a3787049cbd2704d2a363552ae69e8391b34d0
BushraFatima <138645454+BushraF6@users.noreply.github.com>
2023-07-25 22:33:23 +0530

    Solved 4rth problem correctly and updated it.

--- tables.sql
+++ tables.sql
@@ -9,16 +9,16 @@ COPY umpires(umpire,country) FROM '/home/bf/Desktop/database proj/umpires.csv' D
 /* 1. Total runs scored by team. */
 SELECT batting_team,sum(total_runs) FROM deliveries GROUP BY batting_team;
 /* 2. Top batsman for Royal Challengers Bangalore. */
-SELECT batsman FROM( SELECT batsman,sum(batsman_runs) as total_runs_scored FROM deliveries WHERE batting_team='Royal Challengers Bangalore' GROUP BY batsman ORDER BY sum(batsman_runs) DESC LIMIT 10) AS batsmanruns;
+SELECT batsman FROM( SELECT batsman,sum(batsman_runs) AS total_runs_scored FROM deliveries WHERE batting_team='Royal Challengers Bangalore' GROUP BY batsman ORDER BY sum(batsman_runs) DESC LIMIT 10) AS batsmanruns;
 /* 3. Number of Foreign umpires in IPL.*/
-SELECT DISTINCT country,count(country) AS no_of_umpires FROM umpires WHERE country!=' India' GROUP BY country;
+SELECT country,count(country) AS no_of_umpires FROM umpires WHERE country!=' India' GROUP BY country;
 /* 4.Number of matches played by team by season. */
-SELECT season,team1 as team, count(team1)+count(team2) as no_of_matches FROM matches GROUP BY team,season ORDER BY season;
+SELECT season,team, sum(matches_played) FROM (SELECT season,team1 AS team, count(team1) AS matches_played FROM matches GROUP BY team, season UNION SELECT season, team2 AS team, count(team2) AS matches_played FROM matches GROUP BY team,season) AS matchesplayed GROUP BY team,season ORDER BY season;
 /* 5. Number of matches played per year for all the years in IPL. */
 SELECT season,count(season) FROM matches GROUP BY season ORDER BY season;
 /* 6. Number of matches won per team per year in IPL. */
-SELECT season,winner as team, count(winner) as matches_won FROM matches GROUP BY team,season ORDER BY season;
+SELECT season,winner AS team, count(winner) AS matches_won FROM matches GROUP BY team,season ORDER BY season;
 /* 7. Extra runs conceded per team in the year 2016. */
-SELECT bowling_team,sum(extra_runs) as extraruns FROM deliveries JOIN matches ON match_id=id WHERE season=2016 GROUP BY bowling_team ORDER BY sum(extra_runs);
+SELECT bowling_team,sum(extra_runs) AS extraruns FROM deliveries JOIN matches ON match_id=id WHERE season=2016 GROUP BY bowling_team ORDER BY sum(extra_runs);
 /* 8. Top 10 economical bowlers in the year 2015. */
-SELECT bowler,round(economy,2) AS economy_rate FROM(SELECT bowler,sum(total_runs)/(count(over)/6.0) AS economy FROM deliveries JOIN matches ON match_id=id WHERE season=2015 GROUP BY bowler ORDER BY economy) as economyrate LIMIT 10;
+SELECT bowler,round(economy,2) AS economy_rate FROM(SELECT bowler,sum(total_runs)/(count(over)/6.0) AS economy FROM deliveries JOIN matches ON match_id=id WHERE season=2015 GROUP BY bowler ORDER BY economy) AS economyrate LIMIT 10;
